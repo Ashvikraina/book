62a27db02288f15bf48ddd832b70878ddcff6f13
Ashvikraina <138728001+Ashvikraina@users.noreply.github.com>
2024-11-19 22:47:26 +0000

    Everything finished I think

--- src/App.java
+++ src/App.java
@@ -1,27 +1,26 @@
 public class App {
-    public static void main(String[] args)
-    {
-        // Run tests, comment out once they pass.
-        // TestSuite.run();
+    public static void main(String[] args) {
+        // Book 1: Translate "Where the West Begins"
+        TestSuite.run();
+        Book book1 = new Book();
+        book1.readFromUrl("Where the West Begins", "https://www.gutenberg.org/cache/epub/74701/pg74701.txt");
+        int wordCount1 = book1.countWords();
+        System.out.println("Number of words in book 1: " + wordCount1);
 
-        // Starter book
-        Book input = new Book();
+        Book translatedBook1 = PigLatinTranslator.translate(book1);
+        translatedBook1.writeToFile("WhereTheWestBegins_PigLatin.txt");
+        book1.printlines(0, 2);
+        translatedBook1.printlines(0, 2);
 
-        // Read a book from Project Gutenberg (new book URL)
-        input.readFromUrl("New Gutenberg Book", "https://www.gutenberg.org/cache/epub/74701/pg74701.txt");
+        // Book 2: Translate "Romeo and Juliet"
+        Book book2 = new Book();
+        book2.readFromUrl("Romeo and Juliet", "https://www.gutenberg.org/cache/epub/1513/pg1513.txt");
+        int wordCount2 = book2.countWords();
+        System.out.println("Number of words in book 2: " + wordCount2);
 
-        // Count and report words
-        int wordCount = input.countWords();
-        System.out.println("Number of words in book: " + wordCount);
-
-        // Translate the book
-        Book output = PigLatinTranslator.translate(input);
-
-        // Save translated book to file
-        output.writeToFile("NewBook_PigLatin.txt");
-
-        // Print preview lines from original and translated books
-        input.printlines(0, 2);
-        output.printlines(0, 2);
+        Book translatedBook2 = PigLatinTranslator.translate(book2);
+        translatedBook2.writeToFile("RomeoAndJuliet_PigLatin.txt");
+        book2.printlines(0, 2);
+        translatedBook2.printlines(0, 2);
     }
 }
